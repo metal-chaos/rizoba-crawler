f1cfb8b6369d942481d893b9fa85edf719a49600
Yuta Aoki <nightrage.metal66@gmail.com>
2019-10-01 11:36:30 +0900

    added aresort_get_work_info function to modified for getting work time

--- getAresort.py
+++ getAresort.py
@@ -13,29 +13,72 @@ from upsert_mysql import decidePrivatePublish as decidePP
 import traceback,sys
 import random
 import settings
+import pprint #あとで削除
 
 arHomeUrl = settings.AR_HOME_URL
 arListUrl = "https://www.a-resort.jp/resort/ankens/search/?page="
 arDateKey = datetime.datetime.now().strftime('%Y-%m-%d')
 
+###########################
+# TODO:未テストなので要確認！！！！！！
+# targetValue = 仕事内容の中身
+# detailSoup: ページ全体のデータ
+# kindOfElement: 仕事内容のタイトル
+###########################
+def aresort_get_work_info(detailSoup, kindOfElement):
+    # 勤務時間があるか検索
+    targetValue = ""
+    headElement = "<tr>\s*?<th rowspan=\"([0-9]*?)\" scope=\"row\">" + kindOfElement + "<\/th>\s*?<td (class=)?([\s\S]*?)colspan=\"[0-9]*?\">\s*?([\s\S]*?)<\/td>\s*?<\/tr>"
+    addElement = "\s*?<tr>\s*?<td (class=)?([\s\S]*?)colspan=\"[0-9]*?\">\s*?([\s\S]*?)<\/td>\s*?<\/tr>"
+    getDataElement = "<td [class=]?[\s\S]*colspan=\"[0-9]*\">\s*([\s\S]*)<\/td>"
+    newLine = "[\s\S]*"
+    result = re.search(r"<tr>\s*?<th rowspan=\"([0-9]*?)\" scope=\"row\">" + kindOfElement + r"<\/th>\s*?<td (class=)?([\s\S]*?)colspan=\"[0-9]*?\">\s*?([\s\S]*?)<\/td>\s*?<\/tr>", detailSoup)
 
-# （完）求人一覧を取得
+    if (result):
+      addCountForHead = addCountForData = addCountForValue = int(result[1]) - 1
+      entireElement = headElement
+      getAllDataElement = getDataElement
+
+      # （rowspanの数-1）分の正規表現を足す
+      while addCountForHead > 0:
+        addCountForHead += -1
+        entireElement += addElement
+
+      # 仕事内容の中身を取得
+      getArrayValues = re.search(r"" + entireElement + r"", detailSoup)
+      getValue = getArrayValues[0]
+
+      # （rowspanの数-1）分の正規表現を足す
+      while addCountForData > 0:
+        addCountForData += -1
+        getAllDataElement += newLine + getDataElement
+      dataValues = re.search(r"" + getAllDataElement + r"", getValue)
+
+      targetValue = dataValues.group(1)
+      # （rowspanの数-1）分のマッチオブジェクトがあれば<br>で繋ぐ
+      if (addCountForValue > 0):
+        while addCountForValue > 0:
+          targetValue += "<br>" + dataValues.group(addCountForValue + 1)
+          addCountForValue += -1
+    return targetValue
+
+# 求人一覧を取得
 def aresort_page_list(arDateKey):
   arPNumber = 1
-  
+
   while True:
     if arPNumber == 11 or arPNumber == 31:
       arPNumber += 3
     print("アルファ求人一覧「" + str(arPNumber) + "」ページ目の情報を取得中…")
     sleep(random.randint(1,8))
-    
+
     try:
       arHtml = requests.get(arListUrl + str(arPNumber), timeout = 30)
-      arSoup = BeautifulSoup(arHtml.text, 'html.parser') 
+      arSoup = BeautifulSoup(arHtml.text, 'html.parser')
     except:
       dV.exception_error_log()
 
-    # （完）求人詳細へのリンクを全て取得
+    # 求人詳細へのリンクを全て取得
     arAllDtlLink = arSoup.select('div.works_list_link > a')
     for arEachDtlLink in arAllDtlLink:
       arBfDtlLink = re.search(r"(\/resort\/ankens\/view/\?id=\d+)", str(arEachDtlLink))
@@ -46,7 +89,7 @@ def aresort_page_list(arDateKey):
       except:
         dV.exception_error_log()
 
-    # （完）ページネーションから処理終了か判断、item_page_linkへのリンクを全て取得
+    # ページネーションから処理終了か判断、item_page_linkへのリンクを全て取得
     if arPNumber == 1:
       arPNumber += 1
     else:
@@ -57,13 +100,14 @@ def aresort_page_list(arDateKey):
         decidePP.decide_private_publish(arDateKey)
         break
 
-# （完）求人詳細を取得
+# 求人詳細を取得
 def aresort_page_detail(arAfDtlLink, arDateKey):
   print("アルファ求人詳細「" + arAfDtlLink + "」の情報を取得中…")
   sleep(random.randint(1,8))
 
   arDetailHtml = requests.get(arAfDtlLink, timeout = 5)
   arDetailSoup = BeautifulSoup(arDetailHtml.text, 'html.parser')
+  arGetWorkInfo = aresort_get_work_info
 
   # ------------------- 【開始】求人詳細の各要素をスクレイピング -------------------
   arTitle = arDetailSoup.title.string # タイトル
@@ -76,7 +120,8 @@ def aresort_page_detail(arAfDtlLink, arDateKey):
   else:
     arDormitory = "FALSE"
   arPicture = re.search(r"<div id=\"fv\">\s*<img[\s\S]*?src=\"([\s\S]*?)\"", str(arDetailSoup)) # 画像
-  arTime = re.search(r"<th>勤務時間<\/th>\s*<td colspan=\"3\">([\s\S]*?)<\/td>", str(arDetailSoup)) # 勤務時間（pタグ消したい）
+  arTime = arGetWorkInfo(str(arDetailSoup), "勤務時間") # 勤務時間
+  print("arTimeは" + arTime)
   arTreatment = re.search(r"<th>福利厚生<\/th>\s*<td>\s*([\s\S]*?)<\/td>", str(arDetailSoup)) # 待遇
   arJobDesc = re.search(r"<th>仕事内容<\/th>\s*<td colspan=\"3\">([\s\S]*?)<\/td>", str(arDetailSoup)) # 仕事内容
   # resorn用のパーマリンク作成
@@ -104,17 +149,23 @@ def aresort_page_detail(arAfDtlLink, arDateKey):
     arTransportationFee = "FALSE"
   # アフィリエイトリンク付与
   arAffiliateLink = "https://px.a8.net/svt/ejp?a8mat=2HQA4W+4NAW2Y+39C6+BW8O2&a8ejpredirect=https%3A%2F%2Fwww.a-resort.jp%2Fresort%2Fankens%2Fview%2F%3Fid%3D" + str(arUrlNum[1])
+
+  #print("福利厚生は" + arTreatment[1]) TODO:
+
+
   # ------------------- 【終了】求人詳細の各要素をスクレイピング -------------------
 
   # -------------------- 【開始】取得した画像をサーバーに保存する --------------------
   dV.save_image("a-resort", arHomeUrl + arPicture[1], arPermaLink)
   # -------------------- 【終了】取得した画像をサーバーに保存する --------------------
 
+
+
   # "sc_daily"テーブルの実行
-  usDaily.tb_upsert_sc_daily(arAfDtlLink, arTitle, arPermaLink, arDormitory, arHomeUrl + arPicture[1], arOccupation[1], "KindOfSalary無し", int(arSalary[1].replace(',', "")), "時給" + arSalary[1] + arSalary[2], arTerm[1], arTime[1], arTreatment[1], arJobDesc[1], arMeal, arTransportationFee, arWifi, arSpa, arPlace[1], arAffiliateLink, "TRUE", "a-resort", arDateKey)
+  #usDaily.tb_upsert_sc_daily(arAfDtlLink, arTitle, arPermaLink, arDormitory, arHomeUrl + arPicture[1], arOccupation[1], "KindOfSalary無し", int(arSalary[1].replace(',', "")), "時給" + arSalary[1] + arSalary[2], arTerm[1], arTime[1], arTreatment[1], arJobDesc[1], arMeal, arTransportationFee, arWifi, arSpa, arPlace[1], arAffiliateLink, "TRUE", "a-resort", arDateKey)
 
   # "wordpress用のテーブルに反映"
-  toWp.upsert_wp_table(arAfDtlLink, arTitle, arPermaLink, arDormitory, arHomeUrl + arPicture[1], arOccupation[1], "KindOfSalary無し", int(arSalary[1].replace(',', "")), "時給" + arSalary[1] + arSalary[2], arTerm[1], arTime[1], arTreatment[1], arJobDesc[1], arMeal, arTransportationFee, arWifi, arSpa, arPlace[1], arAffiliateLink, "TRUE", "a-resort", arDateKey)
+  #toWp.upsert_wp_table(arAfDtlLink, arTitle, arPermaLink, arDormitory, arHomeUrl + arPicture[1], arOccupation[1], "KindOfSalary無し", int(arSalary[1].replace(',', "")), "時給" + arSalary[1] + arSalary[2], arTerm[1], arTime[1], arTreatment[1], arJobDesc[1], arMeal, arTransportationFee, arWifi, arSpa, arPlace[1], arAffiliateLink, "TRUE", "a-resort", arDateKey)
 
 
 # 関数を実行
