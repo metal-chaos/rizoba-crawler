7a0fd0dc331365097b1849ad00533601af65d7b0
Yuta Aoki <nightrage.metal66@gmail.com>
2020-05-09 12:28:28 +0900

    refectored getHumanic.py likely to object-oriented programming

--- getHumanic.py
+++ getHumanic.py
@@ -7,6 +7,7 @@ from time import sleep
 import pymysql.cursors
 import datetime
 from distinct import distinctValue as dV
+from processing import humanic
 from upsert_mysql import sc_daily as usDaily
 from upsert_mysql import refrectScDataToWp as toWp
 from upsert_mysql import decidePrivatePublish as decidePP
@@ -20,6 +21,11 @@ Global variable
 homeUrl = settings.HU_HOME_URL
 listUrl = "https://www.rizoba.com/search/result/?page="
 dateKey = datetime.datetime.now().strftime('%Y-%m-%d')
+
+#salary
+KIND_OF_SALARY = 0
+NUM_OF_SALARY = 1
+SALARY = 2
 
 def humanic_page_list():
   """
@@ -72,67 +78,67 @@ def humanic_page_detail(afDtlLink):
   datas = {}
 
   # ------------------- 【開始】求人詳細の各要素をスクレイピング -------------------
+  # 初期化
+  processing = humanic.Humanic(str(detailSoup), afDtlLink)
+
   # タイトル
   datas['title'] = detailSoup.title.string
 
   # 勤務地
-  datas['place'] = re.search(r"<dt class=\"item_info_term\">\s*勤務地\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)\s*<\/dd>", str(detailSoup))[1]
+  datas['place'] = processing.place()
 
   # 職種
-  datas['occupation'] = re.search(r"<dt class=\"item_info_term\">\s*職種\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)\s*<\/dd>", str(detailSoup))[1]
+  datas['occupation'] = processing.occupation()
 
   # 勤務期間
-  datas['term'] = re.search(r"<dt class=\"item_info_term\">\s*期間\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)\s*<\/dd>", str(detailSoup))[1]
+  datas['term'] = processing.term()
 
-  # 給与
-  salary = re.search(r"<dt class=\"item_info_term\">\s*(時給|日給|月給)\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)(円)\s*<\/dd>", str(detailSoup))
   # 給与の種類
-  datas['kindOfSalary'] = salary[1]
+  datas['kindOfSalary'] = processing.salary(KIND_OF_SALARY)
   # 給与（数値）
-  datas['numOfSalary'] = salary[2]
+  datas['numOfSalary'] = processing.salary(NUM_OF_SALARY)
   # 給与（掲載用）
-  datas['salary'] = salary[1] + salary[2] + "円"
+  datas['salary'] = processing.salary(SALARY)
 
   # 個室
-  dormitory = re.search(r"<dt class=\"item_info_term\">\s*寮の種類\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)\s*<\/dd>", str(detailSoup))
-  datas['dormitory'] = "TRUE" if ("個室" in dormitory[1]) else "FALSE"
+  datas['dormitory'] = processing.private_room()
 
   # 画像
-  datas['picture'] = re.search(r"<span class=\"item_slide_image\">\s*<img alt=\"[\s\S]*?\" src=\"([\s\S]*?)\"", str(detailSoup))[1]
+  datas['picture'] = processing.picture()
 
   # 勤務時間
-  datas['time'] = re.search(r"<dd class=\"item_info_description work_time_unit\">\s*([\s\S]*?)\s*<\/dd>", str(detailSoup))[1]
+  datas['time'] = processing.time()
 
   # 待遇
-  datas['treatment'] = re.search(r"<dt class=\"item_info_term\">\s*福利厚生\s*<\/dt>\s*<dd class=\"item_info_description\">\s*([\s\S]*?)\s*<\/dd>", str(detailSoup))[1]
+  datas['treatment'] = processing.treatment()
 
   # 仕事内容
-  datas['jobDesc'] = re.search(r"<dt class=\"item_info_lead_term\">\s*仕事内容\s*<\/dt>\s*<dd class=\"item_info_lead_description\">\s*([\s\S]*?)\s*<\/dd>", str(detailSoup))[1]
+  datas['jobDesc'] = processing.jobDesc()
 
   # パーマリンク
-  urlNum = re.search(r"(\d+)", str(afDtlLink))
-  datas['permaLink'] = "detail-humanic-" + str(urlNum[1])
+  datas['permaLink'] = processing.permaLink()
 
   # 食事
-  datas['meal'] = "TRUE" if ("食費無料" in str(detailSoup)) else "FALSE"
+  datas['meal'] = processing.meal()
 
   # wifi
-  datas['wifi'] = "TRUE" if ("食費無料" in str(detailSoup)) else "FALSE"
+  datas['wifi'] = processing.wifi()
 
   # 温泉
-  datas['spa'] = "TRUE" if ("item_merit skin_merit_m2_16" in str(detailSoup)) else "FALSE"
+  datas['spa'] = processing.spa()
 
   # 交通費支給
-  datas['transportationFee'] = "TRUE" if ("交通費支給" in str(detailSoup)) else "FALSE"
+  datas['transportationFee'] = processing.transportationFee()
 
   # アフィリエイトリンク付与
-  datas['affiliateLink'] = "https://px.a8.net/svt/ejp?a8mat=2ZJJHC+5VYEGA+42GS+BW8O2&a8ejpredirect=https%3A%2F%2Fwww.rizoba.com%2Fwork%2F" + str(urlNum[1]) + "%2F"
+  datas['affiliateLink'] = processing.affiliateLink()
 
   # キャンペーン
-  datas['campaign'] = "FALSE"
+  datas['campaign'] = processing.campaign()
 
   # 会社
-  datas['company'] = "humanic"
+  datas['company'] = processing.company()
+
   # ------------------- 【終了】求人詳細の各要素をスクレイピング -------------------
 
   # 取得した画像をサーバーに保存する
---
+++ processing/humanic.py
@@ -0,0 +1,86 @@
+import requests
+import re
+
+class Humanic:
+
+    # private room
+    NO_PRIVATE_ROOM = "FALSE"
+    PRIVATE_ROOM = "TRUE"
+
+    #salary
+    KIND_OF_SALARY = 0
+    NUM_OF_SALARY = 1
+    SALARY = 2
+
+    def __init__(self, soup, dtlLink):
+        self.soup = soup
+        self.dtlLink = dtlLink
+        self.__urlNum = re.search(r"(\d+)", self.dtlLink)
+
+    def title(self):
+        return self.soup.title.string
+
+    def place(self):
+        return re.search(r"<dt class=\"item_info_term\">\s*勤務地\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)\s*<\/dd>", self.soup)[1]
+
+    def term(self):
+        return re.search(r"<dt class=\"item_info_term\">\s*期間\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)\s*<\/dd>", self.soup)[1]
+
+    def occupation(self):
+        return re.search(r"<dt class=\"item_info_term\">\s*職種\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)\s*<\/dd>", self.soup)[1]
+
+    def private_room(self):
+        dormitory = re.search(r"<dt class=\"item_info_term\">\s*寮の種類\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)\s*<\/dd>", self.soup)
+        return Humanic.PRIVATE_ROOM if ("個室" in dormitory[1]) else Humanic.NO_PRIVATE_ROOM
+
+    def salary(self, request):
+        """Get salaries and process it to classify by type
+
+        Args:
+            request (int): type of salary
+        Returns:
+            mixed: processed salary
+        """
+
+        salary = re.search(r"<dt class=\"item_info_term\">\s*(時給|日給|月給)\s*<\/dt>\s*<dd class=\"item_info_description\">\s*(.*)(円)\s*<\/dd>", self.soup)
+        salary = (salary[1] if request == Humanic.KIND_OF_SALARY
+            else salary[2] if request == Humanic.NUM_OF_SALARY
+            else salary[1] + salary[2] + "円" if request == Humanic.SALARY
+            else NULL)
+        return salary
+
+    def picture(self):
+        return re.search(r"<span class=\"item_slide_image\">\s*<img alt=\"[\s\S]*?\" src=\"([\s\S]*?)\"", self.soup)[1]
+
+    def time(self):
+        return re.search(r"<dd class=\"item_info_description work_time_unit\">\s*([\s\S]*?)\s*<\/dd>", self.soup)[1]
+
+    def treatment(self):
+        return re.search(r"<dt class=\"item_info_term\">\s*福利厚生\s*<\/dt>\s*<dd class=\"item_info_description\">\s*([\s\S]*?)\s*<\/dd>", self.soup)[1]
+
+    def jobDesc(self):
+        return re.search(r"<dt class=\"item_info_lead_term\">\s*仕事内容\s*<\/dt>\s*<dd class=\"item_info_lead_description\">\s*([\s\S]*?)\s*<\/dd>", self.soup)[1]
+
+    def permaLink(self):
+        return "detail-humanic-" + str(self.__urlNum[1])
+
+    def meal(self):
+        return "TRUE" if ("食費無料" in self.soup) else "FALSE"
+
+    def wifi(self):
+        return "TRUE" if ("食費無料" in self.soup) else "FALSE"
+
+    def spa(self):
+        return "TRUE" if ("item_merit skin_merit_m2_16" in self.soup) else "FALSE"
+
+    def transportationFee(self):
+        return "TRUE" if ("交通費支給" in self.soup) else "FALSE"
+
+    def affiliateLink(self):
+        return "https://px.a8.net/svt/ejp?a8mat=2ZJJHC+5VYEGA+42GS+BW8O2&a8ejpredirect=https%3A%2F%2Fwww.rizoba.com%2Fwork%2F" + str(self.__urlNum[1]) + "%2F"
+
+    def campaign(self):
+        return "FALSE"
+
+    def company(self):
+        return "humanic"
